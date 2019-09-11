64b82b6f0651a0011e1147f12cb8987bd1794c1f
magdapentek <pentekmagda@yahoo.com>
2019-09-11 20:53:31 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+	// Tipuri de date
+        // String cartype = "Audi";
+        String color = "black";
+        byte speeds = 6;
+        float cylinder = 19.2f;
+        int maxspeed = 260;
+        long consume = 565023440643L;
+
+    }
+}
